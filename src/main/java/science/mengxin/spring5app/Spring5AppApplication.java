b7b9f9fbefeeaead899361adeb131665306262a2
Xin Meng <mengxin.city@gmail.com>
2018-04-23 22:36:38 +0100

    Initial commit

---
+++ src/main/java/science/mengxin/spring5app/Spring5AppApplication.java
@@ -0,0 +1,12 @@
+package science.mengxin.spring5app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5AppApplication.class, args);
+    }
+}
