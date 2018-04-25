88ab97868de4d977beff55e5c09595dfb5d2b41c
Xin Meng <mengxin.city@gmail.com>
2018-04-25 08:14:25 +0100

    095 one to one JPA
    lob

---
+++ src/main/java/science/mengxin/spring5app/domain/Notes.java
@@ -0,0 +1,53 @@
+package science.mengxin.spring5app.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+/**
+ * <p>Date:    25/04/18
+ *
+ * @author mengxin
+ * @version 1.0
+ */
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    //more than 256 char
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
