791b5ceec25d1bb5f9fc18e4a760e421b3cc28cb
mengxin <x.meng@outlook.com>
2018-04-30 22:47:45 +0100

    120 mockito usage

---
+++ src/test/java/science/mengxin/spring5app/services/RecipeServiceImplTest.java
@@ -0,0 +1,46 @@
+package science.mengxin.spring5app.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import science.mengxin.spring5app.domain.Recipe;
+import science.mengxin.spring5app.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        Set<Recipe> recipeSet = recipeService.getRecipes();
+
+        assertEquals(recipeSet.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+//        verify(recipeRepository, times(3)).findAll();
+    }
+}
